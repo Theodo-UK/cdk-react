4b13c9a6675364e62feb5a4ec4cf4f9e696b95d9
Spencer Mehta <spencermehta@gmail.com>
2022-01-13 16:45:13 +0000

    add public hosting

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -9,6 +10,18 @@ export class InfrastructureStack extends Stack {
 
     const bucket = new s3.Bucket(this, "WebsiteBucket", {
       bucketName: "cdk-react-test",
+      websiteIndexDocument: "index.html",
+      blockPublicAccess: new s3.BlockPublicAccess({
+        restrictPublicBuckets: false,
+      }),
     });
+
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      resources: [`${bucket.bucketArn}/*`],
+      principals: [new iam.AnyPrincipal()],
+    });
+
+    bucket.addToResourcePolicy(bucketPolicy);
   }
 }
